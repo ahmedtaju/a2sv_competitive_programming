21276d56176ba8bb9c6cb91a07f1ba7f4ad22229
ahmedtaju <151705759+ahmedtaju@users.noreply.github.com>
2024-11-24 21:16:14 +0300

    Update List Comprehnsion.py

--- hakerRank/List Comprehnsion.py
+++ hakerRank/List Comprehnsion.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-    res=[[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if sum([i,j,k])!=n]
-    print(res)
+    listofNumbers = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if sum([i,j,k])!=n]
+    print(listofNumbers)
